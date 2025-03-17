55c803cfaee3d6af77c2202b8edbb12f6b575c21
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-17 20:44:16 +0100

    feat: font

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,5 @@
+//DAY 1
+
 // Milestone 1
 // Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia (usiamo una qualunque immagine a piacimento)
 // Milestone 2
@@ -11,7 +13,7 @@
 
 
 //Risposta chiamata endpoint link fornito
-[
+const oggetti = [
 	{
 		"id": 1,
 		"title": "Skate Park",
@@ -72,8 +74,8 @@ axios.get(endpoint)
                     <img class="img" src="${arrayElements[i].url}" alt="${arrayElements[i].title}">
                 </div>
                 <div class="testo">
-                    <p>"${arrayElements[i].date}"</p>
-                    <p>"${arrayElements[i].title}"</p>
+                    <p class="date">${arrayElements[i].date}</p>
+                    <p class="title">${arrayElements[i].title}</p>
                 </div>
                 <img class="pin" src="img/pin.svg" alt="Pin">
             </div>
@@ -86,13 +88,16 @@ axios.get(endpoint)
 });
 
 
-{/* <div id="${arrayElements[i].id}" class="box">
-                <div class="immagine">
-                    <img class="img" src="${arrayElements[i].url}" alt="${arrayElements[i].title}">
-                </div>
-                <div class="testo">
-                    <p>${arrayElements[i].date}</p>
-                    <p>${arrayElements[i].title}</p>
-                </div>
-                <img class="pin" src="img/pin.svg" alt="Pin">
-            </div> */}
+//DAY 2
+
+// Milestone 1
+// Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+// Milestone 2
+// Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+// Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+// Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+// Milestone 3
+// Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+// Ci sono diversi modi di farlo, provate a sperimentare :faccia_leggermente_sorridente:
+// Bonus
+// Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
